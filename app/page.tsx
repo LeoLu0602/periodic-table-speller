89138017c01993292f2daae6d8893f652fb54a53
Kenny Cartman <lyuholu@gmail.com>
2024-04-14 11:12:24 +0800

    feat: handle enter key pressed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [sentence, setSentence] = useState<string>('');
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
     setSentence(e.target.value);
   }
+
+  function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>): void {
+    if (e.key === 'Enter') {
+      sendSentence(sentence);
+    }
+  }
+
+  function sendSentence(txt: string) {}
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-green-400 flex flex-col justify-center items-center gap-[80px]">
@@ -18,6 +26,7 @@ export default function Home() {
         className="w-[900px] h-[120px] text-[40px] shadow-2xl focus:outline-none px-[30px] rounded-lg"
         value={sentence}
         onChange={handleChange}
+        onKeyUp={handleKeyUp}
       />
       <div className="w-[900px] h-[120px] text-[40px] bg-green-300 px-[30px] overflow-auto flex items-center rounded-lg">
         {sentence}
