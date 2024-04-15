ef815ecdd97308be9d3e29672249ec12cd3c71fd
Kenny Cartman <lyuholu@gmail.com>
2024-04-15 15:32:44 +0800

    feat: show result array with three different colors (RGB)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 'use client';
 
 import { useState } from 'react';
+import clsx from 'clsx';
+
+interface Response {
+  isBreakable: Boolean;
+  resultArray: string[];
+}
 
 export default function Home() {
   const [sentence, setSentence] = useState<string>('');
+  const [response, setResponse] = useState<Response>({
+    isBreakable: true,
+    resultArray: [],
+  });
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
     setSentence(e.target.value);
@@ -15,7 +25,7 @@ export default function Home() {
     }
   }
 
-  function sendSentence(txt: string) {}
+  function sendSentence(sentence: string) {}
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-green-400 flex flex-col justify-center items-center gap-[80px]">
@@ -28,8 +38,13 @@ export default function Home() {
         onChange={handleChange}
         onKeyUp={handleKeyUp}
       />
-      <div className="w-[900px] h-[120px] text-[40px] bg-green-300 px-[30px] overflow-auto flex items-center rounded-lg">
-        {sentence}
+      <div className="w-[900px] h-[120px] text-[40px] bg-green-300 px-[30px] overflow-auto flex items-center rounded-lg whitespace-pre">
+        {response.isBreakable &&
+          response.resultArray.map((symbol, i) => <div className={clsx('font-bold',{
+            'text-red-500': i % 3 === 0,
+            'text-green-500': i % 3 === 1,
+            'text-blue-500': i % 3 === 2,
+          })}>{symbol}</div>)}
       </div>
     </div>
   );
