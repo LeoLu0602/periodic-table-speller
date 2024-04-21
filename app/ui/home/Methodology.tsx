728228f4c368d65feb0933fde869acbb810fdba6
Kenny Cartman <lyuholu@gmail.com>
2024-04-21 19:49:44 +0800

    feat: add title to Methodology

--- app/ui/home/Methodology.tsx
+++ app/ui/home/Methodology.tsx
@@ -1,3 +1,7 @@
 export default function Methodology() {
-  return <div className='h-screen w-screen bg-green-400'></div>;
+  return (
+    <div className='flex h-screen w-screen justify-center bg-green-400 pt-12'>
+      <h1 className='text-5xl font-bold text-white'>How It Works</h1>
+    </div>
+  );
 }
