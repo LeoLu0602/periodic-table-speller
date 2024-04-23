e3820bb840a4560d1f09cf4478f71eb6cd5eff84
Kenny Cartman <lyuholu@gmail.com>
2024-04-23 19:46:31 +0800

    feat: add PeriodicTable

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Main from '@/app/ui/home/Main';
 import Methodology from '@/app/ui/home/Methodology';
+import PeriodicTable from '@/app/ui/home/PeriodicTable';
 
 export default function Home() {
   return (
-    <main className='pb-96'>
+    <main>
       <Main />
       <Methodology />
+      <PeriodicTable />
     </main>
   );
 }
---
+++ app/ui/home/PeriodicTable.tsx
@@ -0,0 +1,9 @@
+export default function PeriodicTable() {
+  return (
+    <section className='h-screen w-screen pt-8'>
+      <h2 className='mb-8 w-screen text-center text-5xl font-bold text-teal-500'>
+        Periodic Table
+      </h2>
+    </section>
+  );
+}
