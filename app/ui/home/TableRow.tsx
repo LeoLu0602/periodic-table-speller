7b20255961f9cdfced2d7a8528ea2522f8ea14e7
Kenny Cartman <lyuholu@gmail.com>
2024-04-26 19:04:32 +0800

    fix: adjust font size in periodic table

--- app/ui/home/TableRow.tsx
+++ app/ui/home/TableRow.tsx
@@ -11,7 +11,7 @@ export default function TableRow({ elements }: Props) {
         <div
           key={i}
           className={clsx(
-            'relative flex h-[20px] w-[20px] cursor-pointer items-center justify-center text-[10px] font-bold text-white min-[480px]:h-[26px] min-[480px]:w-[26px] min-[768px]:h-[42px] min-[768px]:w-[42px] min-[768px]:text-[20px] min-[1024px]:h-[56px] min-[1024px]:w-[56px]',
+            'relative flex h-[20px] w-[20px] items-center justify-center text-[8px] font-bold text-white min-[480px]:h-[26px] min-[480px]:w-[26px] min-[480px]:text-[10.4px] min-[768px]:h-[42px] min-[768px]:w-[42px] min-[768px]:text-[16.8px] min-[1024px]:h-[56px] min-[1024px]:w-[56px] min-[1024px]:text-[22.4px]',
             {
               'bg-teal-500': number > 0,
             }
